ce98072ee7a57bb78dbc382ba1c31db93f629b11
Pablite5 <Pablite5@GAMING>
2015-10-16 13:30:05 +0200

    final

---
+++ src/bol2/pkg4/Bol24.java
@@ -0,0 +1,27 @@
+
+ 
+package bol2.pkg4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author plagoromani
+ */
+public class Bol24 {
+
+    
+     
+    public static void main(String[] args) {
+      float num1,num2;
+       Scanner obx = new Scanner(System.in);
+        System.out.println("teclea num1 :");
+        num1 = obx.nextFloat();
+        System.out.println("teclea num2 :");
+        num2 = obx.nextFloat();
+        System.out.println("resultado ="+ (num1+num2));
+        
+    
+    }
+    
+}
